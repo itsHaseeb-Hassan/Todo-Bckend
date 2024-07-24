522c842774efc35a5c4da73abcfcb28109d1c5ab
haseeb hassan <its.haseebhassan07@gmail.com>
2024-07-24 23:10:32 +0500

    some changes

--- src/index.js
+++ src/index.js
@@ -5,13 +5,19 @@ import todoRouter from './routes/todoRoute.js'
 import connectDB from './config/db/db.js'
 import cors from 'cors'
 
+const corsconfig={
+   origin:"*",
+   Credentials:true,
+   methods:["GET","POST","DELETE","UPDATE","PUT","PATCH"]
+}
+
 dotenv.config()
 
 const app=express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
-app.use(cors())
+app.use(cors(corsconfig))
 
 app.use('/api/users',userRouter)
 app.use('/api/todos',todoRouter)
