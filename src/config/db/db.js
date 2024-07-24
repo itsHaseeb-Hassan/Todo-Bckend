ef3f15f5cc7996ea97b6068c5601c0466b355be3
haseeb hassan <its.haseebhassan07@gmail.com>
2024-07-25 00:07:46 +0500

    some changes directly

--- src/config/db/db.js
+++ src/config/db/db.js
@@ -6,7 +6,6 @@ const connectDB = async ()=>{
         console.log(`MongoDb connected: ${conn.connection.host}`)
     } catch (error) {
         console.log(`Error:${error.message}`)
-        process.exit(1)
     }
 }
 
