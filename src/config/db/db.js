b8dfbce140035ec6883d851cb61d63463ef3434c
haseeb hassan <its.haseebhassan07@gmail.com>
2024-07-25 13:49:03 +0500

    some changes

--- src/config/db/db.js
+++ src/config/db/db.js
@@ -2,11 +2,9 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try {
-        const conn= await mongoose.connect("mongodb+srv://itshaseebshaukat:admin@cluster0.mkhusfl.mongodb.net/MernTodo",{
+        const conn= await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser: true,
             useUnifiedTopology: true,
-            useFindAndModify: false,
-            useCreateIndex: true
         })
         console.log(`MongoDb connected: ${conn.connection.host}`)
     } catch (error) {
--- src/index.js
+++ src/index.js
@@ -35,21 +35,21 @@ app.get('/', (req, res) => {
 app.use('/api/users', userRouter);
 app.use('/api/todos', todoRouter);
 
-// const startServer = async () => {
-//     try {
-//         const conn = await connectDB();
-//         if(conn){
-//         app.listen(process.env.PORT, () => {
-//             console.log(`Server is running on port ${process.env.PORT}`);
+const startServer = async () => {
+    try {
+        const conn = await connectDB();
+        if(conn){
+        app.listen(process.env.PORT, () => {
+            console.log(`Server is running on port ${process.env.PORT}`);
         
-//         })};
-//     } catch (error) {
-//         console.error('Failed to connect to the database:', error);
-//         process.exit(1); // Exit the process with failure
-//     }
-// };
+        })};
+    } catch (error) {
+        console.error('Failed to connect to the database:', error);
+        process.exit(1); // Exit the process with failure
+    }
+};
 
-// startServer();
+startServer();
 
 app.get("/checkdb", async(req, res) => {
     try {
