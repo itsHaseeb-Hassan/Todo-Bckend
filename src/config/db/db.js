99a6c57e5fcf11e8e4b811d2f837314c0bab674f
haseeb hassan <its.haseebhassan07@gmail.com>
2024-07-25 12:03:49 +0500

    some changes in db.js file

--- src/config/db/db.js
+++ src/config/db/db.js
@@ -2,7 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try {
-        const conn= await mongoose.connect(process.env.MONGO_URI)
+        const conn= await mongoose.connect(process.env.MONGO_URI,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        })
         console.log(`MongoDb connected: ${conn.connection.host}`)
     } catch (error) {
         console.log(`Error:${error.message}`)
