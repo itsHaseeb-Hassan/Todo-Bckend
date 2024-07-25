4b3d541ab67ec229ceba48d0a23ee6f8e4d29345
haseeb hassan <its.haseebhassan07@gmail.com>
2024-07-25 13:10:26 +0500

    some changes

--- src/config/db/db.js
+++ src/config/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async ()=>{
     try {
-        const conn= await mongoose.connect(process.env.MONGO_URI,{
+        const conn= await mongoose.connect("mongodb+srv://itshaseebshaukat:admin@cluster0.mongodb.net/MernTodo?retryWrites=true&w=majority",{
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useFindAndModify: false,
