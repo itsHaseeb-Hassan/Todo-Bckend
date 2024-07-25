581d1f927a0efe1d29516af9c28079eaf0cd2775
haseeb hassan <its.haseebhassan07@gmail.com>
2024-07-25 05:26:01 +0500

    some new changes

--- src/index.js
+++ src/index.js
@@ -33,4 +33,12 @@ app.use('/api/todos',todoRouter)
 connectDB()
 
 
+app.listen(process.env.PORT,()=>{
+    console.log(`Server is running on port ${process.env.PORT}`)
+
+})
+
+export default app
+
+
 
